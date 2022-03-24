0981297a953412059fe9873dcddbc6256617fa63
User_Name <74534100+Gsset@users.noreply.github.com>
2022-03-24 22:50:11 +0300

    fixed maximum count of cores to avoid crashes

--- threading.ps1
+++ threading.ps1
@@ -2,6 +2,7 @@ $docs = Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\
 $ini = Get-Content "$docs\My Games\FalloutNV\Fallout.ini"
 $inidef = Get-Content "$env:fnv_path\Fallout_default.ini"
 $cores = WMIC CPU Get NumberOfCores | select -Skip 2
+if ($cores -gt 6) {$cores = 6}
 Set-ItemProperty -Path "$docs\My Games\FalloutNV\Fallout.ini" -Name IsReadOnly -Value $false
 Set-ItemProperty -Path "$env:fnv_path\Fallout_default.ini" -Name IsReadOnly -Value $false
 $inidef = $inidef -replace 'iNumHWThreads=2', "iNumHWThreads=$cores" -replace 'iNumHavokThreads=2', "iNumHavokThreads=$cores" > "$docs\My Games\FalloutNV\Fallout.ini"
